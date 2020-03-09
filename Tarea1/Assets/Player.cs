95eed3d7e54a91c4cabc1c55db2b422127461bf2
Andre Calderon <andre.calder98@gmail.com>
2020-03-09 12:38:58 -0600

    First Commit

---
+++ Tarea1/Assets/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    private float score = 0;
+    private float v, h;
+    private float vel = 2;
+    private Vector3 pos;
+    public Text txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        StartCoroutine(CheckDeath());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal")*vel;
+        v = Input.GetAxis("Vertical")*vel;
+
+        transform.Translate(h*Time.deltaTime,0,v*Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        
+        if(other.gameObject.tag == "Roller"){
+            this.transform.position = pos;
+            if(score > 0){
+                score = score-1;
+            }
+            txt.GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Meta"){
+            score = score+1;
+            txt.GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+            this.transform.position = pos;        
+        }
+    }
+    IEnumerator CheckDeath(){
+        while (true)
+        {
+        if(transform.position.y <= -1){
+            this.transform.position = pos;
+            if(score > 0){
+                score = score-1;
+            }
+            txt.GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+        }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
---
+++ Tarea1/Assets/Roller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roller : MonoBehaviour
+{
+
+    public float vel;
+    Vector3 startpos;
+    float direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x <= 3.38f ){
+            //print(transform.position.x - startpos.x);
+            if((transform.position.x - 3.38f) <= -6.7f ){
+                //print("Llegaste");
+                direction = -1;
+            }else if(transform.position.x - 3.38f >= -0.2f){
+                direction = 1;
+            }
+            transform.Translate(-direction*Time.deltaTime*vel,0,0);
+        }
+    }
+}
